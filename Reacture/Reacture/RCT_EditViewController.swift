ac443f6d62d4b2d62ae10a0354f769caae3131d4
Benjamin Patch <benjaminarthurpatch@gmail.com>
2016-01-11 12:04:01 -0700

    Storyboard setup
    Setup storyboard and added variables for the views, scrollviews, imageViews, and the constraints for the necessary views.

--- Reacture/Reacture/RCT_EditViewController.swift
+++ Reacture/Reacture/RCT_EditViewController.swift
@@ -12,10 +12,25 @@ class RCT_EditViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.cVToptoToolbarTopConstraint.constant = 100
+//        self.cVToptoToolbarTopConstraint.constant = 100
         self.containerViewController = self.childViewControllers.first! as? RCT_ContainerViewController
         self.containerViewController?.delegate = self
-        SetMockData()
+//        SetMockData()
+        if let rCTImage = self.rCTImage {
+//            self.setupZoomableImageViews(rCTImage)
+            self.frontImageView.image = rCTImage.imageFrontUIImage
+            self.backImageView.image = rCTImage.imageBackUIImage
+            
+        } else {
+            print("ERROR: rCTImage is nil!")
+        }
+        
+        self.frontImageScrollView.minimumZoomScale = 1.0
+        self.frontImageScrollView.maximumZoomScale = 5.0
+        
+        self.backImageScrollView.minimumZoomScale = 1.0
+        self.backImageScrollView.maximumZoomScale = 5.0
+        
     }
     
     func SetMockData() {
@@ -39,6 +54,13 @@ class RCT_EditViewController: UIViewController {
     var rCTImage: RCT_Image?
     var containerViewController: RCT_ContainerViewController?
 
+//    let frontImageZoomableView: UIView = UIView()
+//    let backImageZoomableView: UIView = UIView()
+//    let frontImageScrollView = UIScrollView()
+//    let backImageScrollView = UIScrollView()
+//    let frontImageView = UIImageView()
+//    let backImageView = UIImageView()
+    
     //////////////////////////////
     //////////////////////////////
     // MARK: Functions
@@ -50,18 +72,18 @@ class RCT_EditViewController: UIViewController {
     }
 
     func setUpImages(front: UIImage, back: UIImage){
-        let image1View = UIImageView()
-        image1View.frame.origin.x = self.view.frame.origin.x
-        image1View.frame.size = CGSize(width: self.view.frame.width / CGFloat(2) , height: self.view.frame.height)
-        image1View.contentMode = .ScaleAspectFit
-        self.view.addSubview(image1View)
-        let image2View = UIImageView()
-        image2View.frame.origin.x = self.view.frame.width / 2
-        image2View.frame.size = CGSize(width: self.view.frame.width / CGFloat(2) , height: self.view.frame.height)
-        image2View.contentMode = .ScaleAspectFit
-        self.view.addSubview(image2View)
-        image1View.image = front
-        image2View.image = back
+//        let image1View = UIImageView()
+//        image1View.frame.origin.x = self.view.frame.origin.x
+//        image1View.frame.size = CGSize(width: self.view.frame.width / CGFloat(2) , height: self.view.frame.height)
+//        image1View.contentMode = .ScaleAspectFit
+//        self.view.addSubview(image1View)
+//        let image2View = UIImageView()
+//        image2View.frame.origin.x = self.view.frame.width / 2
+//        image2View.frame.size = CGSize(width: self.view.frame.width / CGFloat(2) , height: self.view.frame.height)
+//        image2View.contentMode = .ScaleAspectFit
+//        self.view.addSubview(image2View)
+//        image1View.image = front
+//        image2View.image = back
     }
 
     //////////////////////////////
@@ -70,14 +92,40 @@ class RCT_EditViewController: UIViewController {
     //////////////////////////////
     //////////////////////////////
 
-    @IBOutlet weak var rCTImageView: UIView!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var layoutButton: UIBarButtonItem!
     @IBOutlet weak var filterButton: UIBarButtonItem!
-    @IBOutlet weak var cVToptoToolbarTopConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var cVToptoToolbarTopConstraint: NSLayoutConstraint!
     //
     //    @IBOutlet weak var cVHeightContraint: NSLayoutConstraint!
+    @IBOutlet weak var rCTImageView: UIView!
+    @IBOutlet weak var frontImageZoomableView: UIView!
+    @IBOutlet weak var frontImageScrollView: UIScrollView!
+    @IBOutlet weak var frontImageView: UIImageView!
+    @IBOutlet weak var backImageZoomableView: UIView!
+    @IBOutlet weak var backImageScrollView: UIScrollView!
+    @IBOutlet weak var backImageView: UIImageView!
 
+    
+    // Constraint Outlets
+    @IBOutlet weak var frontImageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var frontImageWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var frontImageLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var frontImageTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var frontImageTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var frontImageBottomConstraint: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var backImageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backImageWidthConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var backImageLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backImageTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backImageTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backImageBottomConstraint: NSLayoutConstraint!
+    
+    
     //////////////////////////////
     //////////////////////////////
     // MARK: Actions
@@ -115,20 +163,20 @@ class RCT_EditViewController: UIViewController {
     }
 
     func animateContainerView() {
-        if self.cVToptoToolbarTopConstraint.constant == 100 {
-            self.containerView.alpha = 0
-        } else {
-            self.containerView.alpha = 1
-        }
-        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.CurveEaseInOut], animations: { () -> Void in
-            if self.cVToptoToolbarTopConstraint.constant == 100 {
-                self.cVToptoToolbarTopConstraint.constant = 0.1
-            } else {
-                self.cVToptoToolbarTopConstraint.constant = 100
-            }
-            }, completion: {_ in
-                print("Container View Animation Complete")
-        })
+//        if self.cVToptoToolbarTopConstraint.constant == 100 {
+//            self.containerView.alpha = 0
+//        } else {
+//            self.containerView.alpha = 1
+//        }
+//        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.CurveEaseInOut], animations: { () -> Void in
+//            if self.cVToptoToolbarTopConstraint.constant == 100 {
+//                self.cVToptoToolbarTopConstraint.constant = 0.1
+//            } else {
+//                self.cVToptoToolbarTopConstraint.constant = 100
+//            }
+//            }, completion: {_ in
+//                print("Container View Animation Complete")
+//        })
     }
 
 }
@@ -155,3 +203,33 @@ extension RCT_EditViewController: RCT_ContainerViewControllerProtocol {
     }
     
 }
+
+
+
+// MARK: Layout Methods
+extension RCT_EditViewController {
+    
+    
+    func updateWithLayout(layout: Layout) {
+        self.rCTImage?.layout = layout
+        
+        // setup
+        
+        
+        
+    }
+}
+
+
+extension RCT_EditViewController: UIScrollViewDelegate {
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        if scrollView == self.frontImageScrollView {
+            return self.frontImageView
+        } else {
+            return self.backImageView
+        }
+    }
+    
+}
+
