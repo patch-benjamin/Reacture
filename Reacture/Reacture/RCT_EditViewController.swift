d23351dc189dded578b7d0f386ad5b8f77541219
“pkadams67” <paul.k.adams@me.com>
2016-01-13 11:38:11 -0700

    Created filter extension and updated filter function

--- Reacture/Reacture/RCT_EditViewController.swift
+++ Reacture/Reacture/RCT_EditViewController.swift
@@ -23,12 +23,16 @@ class RCT_EditViewController: UIViewController {
 
 
         if let rCTImage = self.rCTImage {
+            //            self.setupZoomableImageViews(rCTImage)
             self.frontImageView.image = rCTImage.imageFrontUIImage
             self.backImageView.image = rCTImage.imageBackUIImage
+
         } else {
             print("ERROR: rCTImage is nil!")
         }
-        
+
+        //        setupController(self.rCTImage)
+
         setupScrollViews()
         setupFilters()
     }
@@ -287,6 +291,7 @@ extension RCT_EditViewController: RCT_ContainerViewControllerProtocol {
             let layoutSelected = Layout(rawValue: indexPath.item)!
             updateWithLayout(layoutSelected)
         } else {
+//            RCT_FiltersController.updateWithFilter(filterSelected, rCTImage: self.rCTImage!)
             let filterSelected = Filter(rawValue: indexPath.item)!
             updateWithFilter(filterSelected)
         }
