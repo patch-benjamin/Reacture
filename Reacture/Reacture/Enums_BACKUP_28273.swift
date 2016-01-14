e53adc05b1415883beabfcbf4970655766892917
“pkadams67” <paul.k.adams@me.com>
2016-01-14 13:30:00 -0700

    New Icons

---
+++ Reacture/Reacture/Enums_BACKUP_28273.swift
@@ -0,0 +1,52 @@
+//
+//  Enums.swift
+//  Reacture
+//
+//  Created by Benjamin Patch on 1/6/16. Amended by Eric Mead on 1/13/16. Amended by Paul Adams on 1/13/16.
+//  Copyright © 2016 PatchWork. All rights reserved.
+//
+
+import Foundation
+
+enum Layout: Int {
+    
+    case TopBottom = 0,
+    LeftRight,
+    PictureInPicture,
+    Count
+}
+
+enum MaskLayout: Int {
+    case None = 0,
+    TopRight,
+    BottomLeft,
+    TopLeft,
+    BottomRight,
+    Top,
+    Bottom,
+    Left,
+    Right,
+    SmallPictureBottomRight,
+    SmallPictureBottomLeft,
+    SmallPictureTopRight,
+    SmallPictureTopLeft
+}
+
+enum Filter: Int {
+    case None = 0,
+    Fade,
+    Chrome,
+<<<<<<< HEAD
+    Poster,
+    Comic,
+    Tonal,
+    Noir,
+=======
+    Comic,
+    Posterize,
+    //    Process,
+    //    Transfer,
+    //    Instant,
+>>>>>>> develop
+    Count
+}
