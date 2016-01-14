07a271f7d9b631004957bdd161aae4df73abdfee
“pkadams67” <paul.k.adams@me.com>
2016-01-13 19:28:52 -0700

    Purtyfied filter buttons

--- Reacture/Reacture/RCT_ContainerViewController.swift
+++ Reacture/Reacture/RCT_ContainerViewController.swift
@@ -12,7 +12,6 @@ var kIsLayoutSelected: Bool? = true
 
 protocol RCT_ContainerViewControllerProtocol {
     func itemSelected(indexPath: NSIndexPath)
-    
 }
 
 class RCT_ContainerViewController: UIViewController {
@@ -36,31 +35,30 @@ class RCT_ContainerViewController: UIViewController {
         if let userInfo = notification.userInfo {
             let arrayOfImageViews = userInfo["filterButtonImageViews"]
             self.arrayOfFilterButtonImageViews = arrayOfImageViews as! [UIImageView]
-            self.collectionView.backgroundColor = UIColor.whiteColor()
             self.collectionView.reloadData()
             print("Reloading Collection View")
         }
     }
 
     func handleReloadCollectionNotification() {
-//        print("Collection View Reloaded")
-//        if kIsLayoutSelected == true {
-//            self.itemCount = Layout.count.rawValue
-//        } else {
-//            // Filter Selected
-//            self.itemCount = Filter.count.rawValue
-//        }
+        //        print("Collection View Reloaded")
+        //        if kIsLayoutSelected == true {
+        //            self.itemCount = Layout.count.rawValue
+        //        } else {
+        //            // Filter Selected
+        //            self.itemCount = Filter.count.rawValue
+        //        }
         collectionView.reloadData()
     }
 
     func setupCollectionView() {
-        collectionView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.75)
+        collectionView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(1)
         if kIsLayoutSelected == true {
             print("Layout is Selected, Present Layout Options")
-//            self.itemCount = self.arrayOfLayoutTitles.count
+            //            self.itemCount = self.arrayOfLayoutTitles.count
         } else {
             print("Filter is Selected, Present Filter Options")
-//            self.itemCount = self.arrayOfFilterTitles.count
+            //            self.itemCount = self.arrayOfFilterTitles.count
         }
     }
 }
@@ -69,16 +67,34 @@ extension RCT_ContainerViewController: UICollectionViewDelegate, UICollectionVie
 
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("OptionItemCell", forIndexPath: indexPath) as! RCT_OptionItemCollectionViewCell
+
+        cell.layer.cornerRadius = 7.5
+        cell.layer.borderColor = UIColor.whiteColor().CGColor
+        cell.layer.borderWidth = 2.0
+
         if kIsLayoutSelected == true {
-            cell.label.text = String(Layout(rawValue: indexPath.item)!)
+            //            cell.label.text = String(Layout(rawValue: indexPath.item)!)
+            cell.label.text = ""
             cell.imageView.backgroundColor = UIColor.whiteColor()
+            let array: [UIImage] = [UIImage(named: "layout_top")!, UIImage(named: "layout_side")!, UIImage(named: "layout_southeast")!]
+            cell.imageView.image = array[indexPath.item]
+
         } else {
             // Filter Selected
-            cell.label.text = String(Filter(rawValue: indexPath.item)!)
+            cell.label.translatesAutoresizingMaskIntoConstraints = true
+            cell.label.textColor = UIColor.whiteColor()
+            cell.label.font = UIFont.systemFontOfSize(18, weight: 5)
+            cell.label.frame.origin = CGPoint(x: 0, y: cell.frame.size.height - cell.label.frame.size.height - 2.5)
+            cell.label.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.3)
+            cell.label.frame.size.width = cell.frame.size.width
+            let labelText = String(Filter(rawValue: indexPath.item)!)
+            cell.label.text = labelText.capitalizedString
             cell.imageView.backgroundColor = UIColor.blackColor()
             // Setting Images for Filter Buttons
             if self.arrayOfFilterButtonImageViews.count == Filter.Count.rawValue {
-                cell.addSubview(arrayOfFilterButtonImageViews[indexPath.item])
+                let imageView = arrayOfFilterButtonImageViews[indexPath.item]
+                let image = imageView.image
+                cell.imageView.image = image
             }
         }
         return cell
@@ -92,7 +108,7 @@ extension RCT_ContainerViewController: UICollectionViewDelegate, UICollectionVie
             return Filter.Count.rawValue
         }
     }
-
+    
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         delegate?.itemSelected(indexPath)
     }
--- Reacture/Reacture/RCT_EditViewController.swift
+++ Reacture/Reacture/RCT_EditViewController.swift
@@ -309,7 +309,7 @@ extension RCT_EditViewController {
         let fadeFilterName = "CIPhotoEffectFade"
         let chromeFilterName = "CIPhotoEffectChrome"
         let comicFilterName = "CIComicEffect"
-        let posterizeFilterName = "CIColorPosterize"
+        let posterFilterName = "CIColorPosterize"
 
         // Possible Future Filters Not in Use:
         //        let processFilterName = ""
@@ -342,9 +342,9 @@ extension RCT_EditViewController {
             case .Comic:
                 print("Comic Filter Selected")
                 performFilter(comicFilterName)
-            case .Posterize:
-                print("Posterize Filter Selected")
-                performFilter(posterizeFilterName)
+            case .Poster:
+                print("Poster Filter Selected")
+                performFilter(posterFilterName)
                 //            case .Process:
                 //                print("Process Filter Selected")
                 //                performFilter(processFilterName)
@@ -399,7 +399,7 @@ extension RCT_EditViewController {
         let fadeFilterName = "CIPhotoEffectFade"
         let chromeFilterName = "CIPhotoEffectChrome"
         let comicFilterName = "CIComicEffect"
-        let posterizeFilterName = "CIColorPosterize"
+        let posterFilterName = "CIColorPosterize"
 
         if self.rCTImage != nil {
 
@@ -426,9 +426,9 @@ extension RCT_EditViewController {
             case .Comic:
                 print("Comic Filter Selected")
                 performThumbnailFilter(comicFilterName)
-            case .Posterize:
-                print("Posterize Filter Selected")
-                performThumbnailFilter(posterizeFilterName)
+            case .Poster:
+                print("Poster Filter Selected")
+                performThumbnailFilter(posterFilterName)
             case .Count:
                 print("Count Enum")
                 break
